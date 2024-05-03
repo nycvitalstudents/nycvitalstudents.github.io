c6432fcca5775dfe2ab7eb0494ee854e8114e285
sjorense <99450081+sjorense@users.noreply.github.com>
2024-05-03 10:50:39 -0400

    March-April edits

--- js/slideshow3.js
+++ js/slideshow3.js
@@ -10,5 +10,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 15000); // Changes image every 15 seconds
+  setTimeout(showSlides, 10000); // Changes image every 15 seconds
 }
